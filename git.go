e090375f44c92ebfda377c0601d15adbb81868de
m_nakamura145 <masato.nakamura145@gmail.com>
2016-07-04 22:47:49 +0900

    First commit

---
+++ git.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func RemoteHeadCommitId() (string, error) {
+	cmd := exec.Command("git", "ls-remote", "origin", "HEAD")
+	cmd.Stderr = os.Stderr
+
+	id, err := cmd.Output()
+
+	if err != nil {
+		return "", err
+	}
+
+	return strings.Split(string(id), "\t")[0], nil
+}
+
+func LocalHeadCommitId() (string, error) {
+	cmd := exec.Command("git", "show", "HEAD", "-s", "--format=%H")
+	cmd.Stderr = os.Stderr
+
+	id, err := cmd.Output()
+
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(id)), nil
+}
+
+func IsLatest(localId, remoteId string) bool {
+	if localId == "" || remoteId == "" {
+		return false
+	}
+	return localId == remoteId
+}
